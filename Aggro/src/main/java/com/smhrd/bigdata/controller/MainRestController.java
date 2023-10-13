2f4cac8fc50402b4e7e980cc6f6bc7738f82cbf4
오승원 <netakasa@gmail.com>
2023-10-13 15:46:51 +0900

    2023-10-13 15:46 오승원 post 회원정보 전송 테스트 완료

--- Aggro/src/main/java/com/smhrd/bigdata/controller/MainRestController.java
+++ Aggro/src/main/java/com/smhrd/bigdata/controller/MainRestController.java
@@ -1,7 +1,13 @@
 package com.smhrd.bigdata.controller;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.smhrd.bigdata.model.UserInfo;
 
 @RestController
 public class MainRestController {
@@ -12,5 +18,18 @@ public class MainRestController {
 		System.out.println("서버 접속함");
 		
 		return "통신됨";
+	}
+	
+	@PostMapping("/form")
+	@ResponseBody
+	public String axiosForm(@RequestBody UserInfo userInfo) {
+		
+		System.out.println("데이터 통신 연결됨");
+		System.out.println(userInfo.getUser_id());
+		System.out.println(userInfo.getUser_pw());
+		System.out.println(userInfo.getUser_nick());
+		
+		
+		return "데이터 넘어옴";
 	}
 }
